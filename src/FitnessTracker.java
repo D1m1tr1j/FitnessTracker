f268477f0b65ab638bb6e70e26c39ed8146da4d3
Schnebel, Alexander <alexander@schnebel.dev>
2021-11-06 22:03:38 +0100

    Make public methods private

--- src/FitnessTracker.java
+++ src/FitnessTracker.java
@@ -20,20 +20,6 @@ public class FitnessTracker {
                 "Current energy level is " + energyLevel + " Watt." + getAdditionalInfo();
     }
 
-    public String getAdditionalInfo() {
-        String additionalInfo = "";
-
-        if (isNightTime(getCurrentHour())) {
-            additionalInfo = " Note: currently night time!";
-        } else if (energyLevel > 150) {
-            additionalInfo = " Note: currently high energy level!";
-        } else if (energyLevel < 50) {
-            additionalInfo = " Note: currently low energy level!";
-        }
-
-        return additionalInfo;
-    }
-
     public String getName() {
         return name;
     }
@@ -66,7 +52,40 @@ public class FitnessTracker {
         return heartbeat;
     }
 
-    public void setHeartbeat() {
+    public int getActivityLevel() {
+        return activityLevel;
+    }
+
+    public int getActivityTrend() {
+        return activityTrend;
+    }
+
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
+
+    public void goOn() {
+        setActivityLevel();
+        setHeartbeat();
+        setEnergyLevel();
+    }
+
+    // I made the methods private because a user shall not be able to access or call the methods below
+    // inside the main() function. I treat them like the inner workings of a clock - or in this case -
+    // a fitness tracker :D
+
+    // Watch closely - although this method is a 'set', it doesn't mean you must provide a parameter
+    // The document states, that the function doesn't have a parameter
+    private void setActivityTrend() {
+        activityTrend += (int) (20 * Math.random() - 10);
+    }
+
+    // Same here
+    private void setEnergyLevel() {
+        energyLevel = (int) (heartbeat * activityLevel / 32.1);
+    }
+
+    private void setHeartbeat() {
         int currentHour = getCurrentHour();
         if (isNightTime(currentHour)) {
             heartbeat = (int) (45 + (20 * Math.random() - 10));
@@ -75,45 +94,30 @@ public class FitnessTracker {
         }
     }
 
-    public int getActivityLevel() {
-        return activityLevel;
-    }
-
-    public void setActivityLevel() {
+    private void setActivityLevel() {
         if (activityLevel < activityTrend) {
             activityLevel += 5;
         } else if (activityLevel > activityTrend) {
             activityLevel -= 4;
-        // This if statement can be removed and you can leave a simple else here
-        // If neither of both statements above are true, both values must be identical
+            // This if statement can be removed and you can leave a simple else here
+            // If neither of both statements above are true, both values must be identical
         } else if (activityLevel == activityTrend) {
             setActivityTrend();
         }
     }
 
-    public int getActivityTrend() {
-        return activityTrend;
-    }
+    private String getAdditionalInfo() {
+        String additionalInfo = "";
 
-    // Watch closely - although this method is a 'set', it doesn't mean you must provide a parameter
-    // The document states, that the function doesn't have a parameter
-    public void setActivityTrend() {
-        activityTrend += (int) (20 * Math.random() - 10);
-    }
+        if (isNightTime(getCurrentHour())) {
+            additionalInfo = " Note: currently night time!";
+        } else if (energyLevel > 150) {
+            additionalInfo = " Note: currently high energy level!";
+        } else if (energyLevel < 50) {
+            additionalInfo = " Note: currently low energy level!";
+        }
 
-    public int getEnergyLevel() {
-        return energyLevel;
-    }
-
-    // Same here
-    public void setEnergyLevel() {
-        energyLevel = (int) (heartbeat * activityLevel / 32.1);
-    }
-
-    public void goOn() {
-        setActivityLevel();
-        setHeartbeat();
-        setEnergyLevel();
+        return additionalInfo;
     }
 }
 
